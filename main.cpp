00e6f67b007ef88f3e79b1dfe5d5280241ee1258
guoce <hfpgce@gmail.com>
2025-08-19 13:27:56 +0800

    Add Clipper2 library integration and extend example usage with Eigen

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <version>
+#include <Eigen/Core>
+#include <clipper2/clipper.h>
 
 int main()
 {
-    std::cout<<"hello world"<<std::endl;
+    std::cout << "hello world" << std::endl;
+    Eigen::Vector3d v(1, 2, 3);
+    Eigen::Vector3d v2(1, 2, 3);
+    //v2.normalize();
+    std::cout << v2.norm() << std::endl;
+    Eigen::Matrix3d v3;
+    v3.setZero();
+    std::cout << v3 << std::endl;
+    v3.col(0) = v;
+    std::cout << v3 << std::endl;
     return 0;
 }
